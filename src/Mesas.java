ec98054ed3f188649a73183998d8cc5e5d533e06
Thiago Sosa <sosat279@gmail.com>
2025-09-05 22:49:14 -0300

    Primeros cambios

---
+++ src/Mesas.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Mesas {
+    private JPanel JPMesasIni;
+    private JTabbedPane JPPedidos;
+    private JPanel JPprincipal;
+    private JPanel JPasignar;
+    private JPanel JPpedidos;
+    private JLabel lblMesa;
+    private JLabel lblSelecEstado;
+    private JLabel lblEstado;
+}
