3f522f61ed36419909b666c3b0d305b8aae16254
Costa200513 <costaezequiel56@gmail.com>
2025-09-10 13:48:44 -0300

    Agregado base de conexion a base de datos

--- src/DAO/ConexionDB.java
+++ src/DAO/ConexionDB.java
@@ -4,13 +4,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConexionDB {
-    private static final String URL  =
-            "jdbc:mysql://localhost:3306/nombre";
+public class ConexionDB
+{
+    private static final String URL  = "jdbc:mysql://localhost:3306/nombre";
     private static final String USER = "root";
     private static final String PASS = "1234";
 
-    public static Connection getConnection() throws SQLException {
+    public static Connection getConnection() throws SQLException
+    {
         return DriverManager.getConnection(URL, USER, PASS);
     }
 }
