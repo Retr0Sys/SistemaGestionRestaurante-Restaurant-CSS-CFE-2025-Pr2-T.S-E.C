911a369857c98802924ea9306f1680e28cda62ed
Costa200513 <costaezequiel56@gmail.com>
2025-09-10 13:43:19 -0300

    MOdificacion form Mesa y su clase

--- src/Forms/Mesa.java
+++ src/Forms/Mesa.java
@@ -40,6 +40,15 @@ public class Mesa extends  JFrame {
     private JLabel lblEstadoFin;
     private JButton btnAtras;
     private JLabel lblMesas;
+    private JLabel lblMesa4;
+    private JLabel lblOperacionReserva;
+    private JLabel lblCanitdadPersonas;
+    private JTextField txtCantidadPersonas;
+    private JLabel lblFecha;
+    private JLabel lblCalendario;
+    private JPanel JPReservas;
+    private JRadioButton rbtnAñadir;
+    private JRadioButton rbtnEliminar;
 
 
     public Mesa(){
